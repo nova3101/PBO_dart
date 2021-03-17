e090965b8eb10a993fdc07fdac783ca1491479bd
nova3101 <novacantikafitri@gmail.com>
2021-03-17 17:23:17 +0700

    pbo dart

---
+++ pbo_dasar_dart/bin/pbo_dasar_dart.dart
@@ -0,0 +1,5 @@
+import 'package:pbo_dasar_dart/pbo_dasar_dart.dart' as pbo_dasar_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pbo_dasar_dart.calculate()}!');
+}
---
+++ pbo_dasar_dart/test/pbo_dasar_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:pbo_dasar_dart/pbo_dasar_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
